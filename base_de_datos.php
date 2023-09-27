a7c610a5e2dabfebee429cecb545ed0f9c214781
YenferVargas <yenifer.vargas@tecsup.edu.pe>
2023-09-27 01:27:02 -0500

    1

--- base_de_datos.php
+++ base_de_datos.php
@@ -1,24 +1,16 @@
 <?php
-$database_url = getenv('postgresql://postgres:HA23SIzJFNGvxBH68oRN@containers-us-west-108.railway.app:6753/railway');
+$contraseña = "HA23SIzJFNGvxBH68oRN";
+$usuario = "postgres";
+$nombreBaseDeDatos = "Mesas";
 
-if ($database_url === false) {
-    die("La variable de entorno DATABASE_URL no está configurada correctamente.");
-}
-
-try {
-    $db_params = parse_url($database_url);
-    
-    $db_host = $db_params['host'];
-    $db_port = $db_params['port'];
-    $db_name = ltrim($db_params['path'], '/');
-    $db_user = $db_params['user'];
-    $db_password = $db_params['pass'];
-
-    $base_de_datos = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_password);
+$rutaServidor = "postgresql://postgres:HA23SIzJFNGvxBH68oRN@containers-us-west-108.railway.app:6753/railway";
+$puerto = "6753";
+try{
+    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
     $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa a la base de datos.";
-} catch (PDOException $e) {
-    die("Error de conexión a la base de datos: " . $e->getMessage());
+} catch (Exception $e) {
+    echo("Error de conexión a la base de datos: " . $e->getMessage());
 }
+
 
 ?>
