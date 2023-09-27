cbd470426a7c0f035bd8ef2ab85ed4ce498ed17a
YenferVargas <yenifer.vargas@tecsup.edu.pe>
2023-09-27 00:28:36 -0500

    cambio6

--- insertar.php
+++ insertar.php
@@ -4,14 +4,14 @@ Este archivo inserta los datos enviados a través de formulario.php
 */
 ?>
 <?php
-#Salir si alguno de los datos no está presente
+# Salir si alguno de los datos no está presente
 if (!isset($_POST["nombre"]) || !isset($_POST["edad"])) {
     exit();
 }
 
-#Si todo va bien, se ejecuta esta parte del código...
+# Si todo va bien, se ejecuta esta parte del código...
 
-include_once "base_de_datos.php";
+include_once "base_de_datos.php"; // Asegúrate de que el nombre del archivo sea correcto
 $nombre = $_POST["nombre"];
 $edad = $_POST["edad"];
 
@@ -20,15 +20,19 @@ Al incluir el archivo "base_de_datos.php", todas sus variables están
 a nuestra disposición. Por lo que podemos acceder a ellas tal como si hubiéramos
 copiado y pegado el código
  */
-$sentencia = $base_de_datos->prepare("INSERT INTO mascotas(nombre, edad) VALUES (?, ?);");
-$resultado = $sentencia->execute([$nombre, $edad]); # Pasar en el mismo orden de los ?
+if (isset($base_de_datos)) { // Verifica que $base_de_datos esté definida
+    $sentencia = $base_de_datos->prepare("INSERT INTO mascotas(nombre, edad) VALUES (?, ?);");
+    $resultado = $sentencia->execute([$nombre, $edad]); # Pasar en el mismo orden de los ?
 
-#execute regresa un booleano. True en caso de que todo vaya bien, falso en caso contrario.
-#Con eso podemos evaluar
-
-if ($resultado === true) {
-    # Redireccionar a la lista
-	header("Location: listar.php");
+    # execute regresa un booleano. True en caso de que todo vaya bien, falso en caso contrario.
+    # Con eso podemos evaluar
+    if ($resultado === true) {
+        # Redireccionar a la lista
+        header("Location: listar.php");
+    } else {
+        echo "Algo salió mal. Por favor verifica que la tabla exista";
+    }
 } else {
-    echo "Algo salió mal. Por favor verifica que la tabla exista";
+    echo "Error: La conexión a la base de datos no está definida en 'base_de_datos.php'";
 }
+?>
