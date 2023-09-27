663f2ee86d657c80b55379b143d795e6bedbe7ef
YenferVargas <yenifer.vargas@tecsup.edu.pe>
2023-09-27 01:29:34 -0500

    2

--- base_de_datos.php
+++ base_de_datos.php
@@ -3,13 +3,15 @@ $contraseña = "HA23SIzJFNGvxBH68oRN";
 $usuario = "postgres";
 $nombreBaseDeDatos = "Mesas";
 
-$rutaServidor = "postgresql://postgres:HA23SIzJFNGvxBH68oRN@containers-us-west-108.railway.app:6753/railway";
+$rutaServidor = "containers-us-west-108.railway.app"; // Solo el nombre del host
 $puerto = "6753";
-try{
+
+try {
     $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
     $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    echo("Error de conexión a la base de datos: " . $e->getMessage());
+    echo "Conexión exitosa a la base de datos.";
+} catch (PDOException $e) {
+    die("Error de conexión a la base de datos: " . $e->getMessage());
 }
 
 
